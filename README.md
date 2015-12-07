ecdd0350731b82abe833f72d61c3f145f3f83282
Victorio Berra <toryberra@gmail.com>
2015-12-07 16:50:45 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# tennu-wibble
+
+A plugin for the [tennu](https://github.com/Tennu/tennu) irc framework.
+
+Requested by members of my IRC channel. Responds to wibble, wobble and jelly when said in the correct order inside a pre-defined trime limit. Compatible with [tennu-admin-cooldown](https://github.com/LordWingZero/tennu-admin-cooldown).
+
+### Configuration
+```` Javascript
+"wibble": {
+    "cooldown": 10
+    "expires": 15
+}
+````
+
+### Requires
+- [tennu-admin](https://github.com/Tennu/tennu-admin)
+
+### Installing Into Tennu
+
+See Downloadable Plugins [here](https://tennu.github.io/plugins/).
+
+### Todo:
+
+- Tests
---
+++ plugin.js
@@ -0,0 +1,95 @@
+var moment = require('moment');
+
+const requiresAdminHelp = "Requires admin privileges.";
+
+var WibblePlugin = {
+    requiresRoles: ["admin"],
+    init: function(client, imports) {
+
+        var wibbleWobbleJellyCache = [];
+
+        var wibbleConfig = client.config("wibble");
+        if (!wibbleConfig || !wibbleConfig.expires) {
+            throw Error("tennu-wibble: is missing some or all of its configuration.");
+        }
+
+        var isAdmin = imports.admin.isAdmin;
+        const adminCooldown = client._plugins.getRole("cooldown");
+        if (adminCooldown) {
+            var cooldown = wibbleConfig['cooldown'];
+            if (!cooldown) {
+                client._logger.warn('tennu-wibble: Cooldown plugin found but no cooldown defined.')
+            }
+            else {
+                isAdmin = adminCooldown(cooldown);
+                client._logger.notice('tennu-wibble: cooldowns enabled: ' + cooldown + ' seconds.');
+            }
+        }
+
+        function WibbleWobbleCheck(IRCMessage) {
+
+            return isAdmin(IRCMessage.hostmask).then(function(isadmin) {
+
+                // isadmin will be "undefined" if cooldown system is enabled
+                // isadmin will be true/false if cooldown system is disabled
+                if (typeof(isadmin) !== "undefined" && isadmin === false) {
+                    throw new Error(requiresAdminHelp);
+                }
+
+                if (IRCMessage.message.toLowerCase() !== 'wibble' &&
+                    IRCMessage.message.toLowerCase() !== 'wobble' &&
+                    IRCMessage.message.toLowerCase() !== 'jelly') {
+                    return;
+                }
+
+                var currentverb = IRCMessage.message.toLowerCase();
+                var existingRequest = wibbleWobbleJellyCache[IRCMessage.nickname.toLowerCase()];
+
+                var response = '';
+
+                // Expire out an old verb
+                if (existingRequest && (moment().subtract(wibbleConfig.expires, 'seconds')) > existingRequest.timestamp) {
+                    existingRequest.lastSaidVerb = 'jelly';
+                }
+
+                if (currentverb === 'wibble' && (!existingRequest || existingRequest.lastSaidVerb === 'jelly')) {
+                    response = 'wobble';
+                }
+                else if (currentverb === 'wobble' && existingRequest.lastSaidVerb === 'wibble') {
+                    response = 'jelly';
+                }
+                else if (currentverb === 'jelly' && existingRequest.lastSaidVerb === 'wobble') {
+                    response = 'Now you\'re getting it!';
+                }
+
+                if (response !== '') {
+                    // Update the user/verb store                
+                    wibbleWobbleJellyCache[IRCMessage.nickname.toLowerCase()] = {
+                        timestamp: moment(),
+                        lastSaidVerb: currentverb
+                    };
+
+                    return response;
+                }
+
+            }).catch(adminFail);
+        }
+
+        function adminFail(err) {
+            return {
+                intent: 'notice',
+                query: true,
+                message: err
+            };
+        }
+
+        return {
+            handlers: {
+                "privmsg": WibbleWobbleCheck,
+            }
+        };
+
+    }
+};
+
+module.exports = WibblePlugin;
