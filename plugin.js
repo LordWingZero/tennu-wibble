45025de993907327c6eaf01fdae0b269194653f5
Victorio Berra <toryberra@gmail.com>
2016-01-11 08:49:50 -0600

    Use new configDefaults

--- plugin.js
+++ plugin.js
@@ -1,53 +1,55 @@
 var moment = require('moment');
 
 var WibblePlugin = {
+    configDefaults: {
+        "wibble": {
+            "expires": 15
+        }
+    },
     init: function(client, imports) {
 
         var wibbleWobbleJellyCache = [];
 
         var wibbleConfig = client.config("wibble");
-        if (!wibbleConfig || !wibbleConfig.expires) {
-            throw Error("tennu-wibble: is missing some or all of its configuration.");
-        }
 
         function WibbleWobbleCheck(IRCMessage) {
 
-                if (['wibble','wobble','jelly'].indexOf(IRCMessage.message.toLowerCase()) === -1) {
-                    return;
-                }
+            if (['wibble', 'wobble', 'jelly'].indexOf(IRCMessage.message.toLowerCase()) === -1) {
+                return;
+            }
 
-                var currentverb = IRCMessage.message.toLowerCase();
-                var existingRequest = wibbleWobbleJellyCache[IRCMessage.nickname.toLowerCase()];
+            var currentverb = IRCMessage.message.toLowerCase();
+            var existingRequest = wibbleWobbleJellyCache[IRCMessage.nickname.toLowerCase()];
 
-                var response = '';
+            var response = '';
 
-                // Expire out an old verb
-                if (existingRequest && (moment().subtract(wibbleConfig.expires, 'seconds')) > existingRequest.timestamp) {
-                    existingRequest.lastSaidVerb = 'jelly';
-                }
+            // Expire out an old verb
+            if (existingRequest && (moment().subtract(wibbleConfig.expires, 'seconds')) > existingRequest.timestamp) {
+                existingRequest.lastSaidVerb = 'jelly';
+            }
 
-                if (currentverb === 'wibble' && (!existingRequest || existingRequest.lastSaidVerb === 'jelly')) {
-                    response = 'wobble';
-                }
-                else if (currentverb === 'wobble' && existingRequest.lastSaidVerb === 'wibble') {
-                    response = 'jelly';
-                }
-                else if (currentverb === 'jelly' && existingRequest.lastSaidVerb === 'wobble') {
-                    response = 'Now you\'re getting it!';
-                }
-                else if (currentverb === 'jelly' && existingRequest.lastSaidVerb === 'wobble') {
-                    response = 'Now you\'re getting it!';
-                }
+            if (currentverb === 'wibble' && (!existingRequest || existingRequest.lastSaidVerb === 'jelly')) {
+                response = 'wobble';
+            }
+            else if (currentverb === 'wobble' && existingRequest.lastSaidVerb === 'wibble') {
+                response = 'jelly';
+            }
+            else if (currentverb === 'jelly' && existingRequest.lastSaidVerb === 'wobble') {
+                response = 'Now you\'re getting it!';
+            }
+            else if (currentverb === 'jelly' && existingRequest.lastSaidVerb === 'wobble') {
+                response = 'Now you\'re getting it!';
+            }
 
-                if (response !== '') {
-                    // Update the user/verb store                
-                    wibbleWobbleJellyCache[IRCMessage.nickname.toLowerCase()] = {
-                        timestamp: moment(),
-                        lastSaidVerb: currentverb
-                    };
+            if (response !== '') {
+                // Update the user/verb store                
+                wibbleWobbleJellyCache[IRCMessage.nickname.toLowerCase()] = {
+                    timestamp: moment(),
+                    lastSaidVerb: currentverb
+                };
 
-                    return response;
-                }
+                return response;
+            }
         }
 
         return {
